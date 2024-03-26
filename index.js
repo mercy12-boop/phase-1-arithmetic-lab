81810a525a01c4f8d44903ccdaf0d1ebb62d586f
Mercy Wanjiru <wanjirumercy54@gmail.com>
2024-03-26 13:14:12 +0300

    Completes arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+// console.log(multiply);
+
+const random = Math.round(Math.random() * 10) + 1;
+// console.log(random);
+
+const num3 = 54;
+const num4 = 5;
+const mod = num3 % num4;
+// console.log(mod);
+
+const max = Math.max(20, 19, 18, 17);
+console.log(max);
